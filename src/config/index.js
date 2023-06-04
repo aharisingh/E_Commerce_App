3618c4eed9bd244bac11e96a42270dcfef415cbf
Harivansh Narayan Singh <harivanshkashyap04@gmail.com>
2023-06-04 14:04:40 +0000

    added forgot Password functionality

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,8 @@ dotenv.config()
 
 const config = {
     PORT : process.env.PORT || 5000 ,
-    MONGODB_URL : process.env.MONGODB_URL || "mongodb://localhost:27017/ecommerce"
+    MONGODB_URL : process.env.MONGODB_URL || "mongodb://localhost:27017/ecommerce" , 
+    JWT_SECRET : process.env.JWT_SECRET || "mysecret",
+    JWT_EXPIRY : process.env.JWT_EXPIRY || "5d"
 }
 export default config
--- src/models/user.schema.js
+++ src/models/user.schema.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose"
 import AuthRoles from "../utils/authRoles.js"
 import bcrypt from "bcryptjs"
+import JWT from "jsonwebtoken"
+import { config } from "dotenv"
+import crypto from "crypto"
 
 const {Schema} =  mongoose
 
@@ -41,6 +44,23 @@ userSchema.methods = {
     //compare password
     comparePassword : async function(enteredPassword){
         return await bcrypt.hash(enteredPassword , this.password)
+    },
+    getJWTToken :  function(){
+        JWT.sign({_id : this._id , role : this.role} , config.JWT_SECRET, {
+            expiresIn :  config.JWT_EXPIRY
+        })
+    },
+    //generate forgot password
+    generateForgotPasswordToken  :  function (){
+        //token creation
+        const forgotToken = crypto.randomBytes(20).toString("hex")
+
+        //encrypt the token
+        this.forgotPasswordToken =  crypto.createHash("sha256").update(forgotToken).digest("hex")
+
+        //token life time (10 mins)
+        this.forgotPasswordExpiry = Date.now() + 10*60*1000
+        return forgotToken
     }
 }
 
